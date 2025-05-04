9934bb59752a57aa8ffb03648484876280bbf2e8
taeminers <taeminers@gmail.com>
2025-05-04 23:03:55 +0900

    chore: tailwind v4 no longer uses configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div>
-      Static
-      <br />
-      <Link href="dashboard">Dashboard Page</Link>
-      <br />
-      <Link href="about">About Page</Link>
-      <Link href="wowzers">Cookies</Link>
+    <div className="flex-center">
+      <h1>Hello</h1>
     </div>
   );
 }
