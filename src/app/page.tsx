3b3aba119719c94d1a6b49be3b9406ecb008189b
taeminers <taeminers@gmail.com>
2025-05-04 23:43:07 +0900

    design: add shadcn components and css templates for tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex-center">
-      <h1>Hello</h1>
+      <h1 className="rounded-zm">Hello</h1>
+      <Button>Log in</Button>
     </div>
   );
 }
