a5ffec81593925f6fde9aa330b7ae947c98ab150
taeminers <taeminers@gmail.com>
2025-05-05 23:10:12 +0900

    chore: clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div className="flex-center">
-      <h1 className="rounded-zm">Hello</h1>
-      <Button>Log in</Button>
-    </div>
-  );
+  return <div></div>;
 }
